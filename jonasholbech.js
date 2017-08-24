f287733bf84a78114add69bd6579c8450259e546
Jonas Holbech <jh@jonasholbech.dk>
2017-08-24 10:50:04 +0200

    initial commit

---
+++ jonasholbech.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Jonas Holbech",
+    birthday: {
+        year: 1978,
+        month: 8,
+        day: 6
+    },
+    sex: "male",
+    spiritAnimal: "Were Cat",
+    skills: ["JS", "Drinking beer", "Shouting"]
+}
